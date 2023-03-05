6fa0f24a75f63e526ab133677741d33465657a09
Risman Hidayatulloh <risman100@gmail.com>
2023-03-05 16:30:14 +0700

    quiz learn install package

---
+++ node-package-manager/index.js
@@ -0,0 +1,7 @@
+const _ = require('lodash')
+
+const myOddEventArray = _.partition([1, 2, 3, 4, 5, 6], (n) => n % 2)
+
+console.log(myOddEventArray)
+
+//compile : node ./node-package-manager/index.js
